896cc6477fbba53dcd5601bc88f64e6190c79f93
Steve Brown <steve.brown@thebrownnet.co.uk>
2020-04-30 18:18:58 +0100

    work around getting the puppet stuff for windows working. THIS IS NOT WORKING DO NOT USE!

--- puppet-windows.ps1
+++ puppet-windows.ps1
@@ -26,10 +26,15 @@ param (
     [string]
     $DomainName = "local",
 
-    # The version of Puppet to install
+    # The major version of Puppet to install
     [Parameter(Mandatory = $false)]
     [string]
     $PuppetVersion = "puppet6",
+
+    # The specific puppet agent version to install
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PuppetAgentVersion = 'latest',
 
     # The port to connect to on the master
     [Parameter(Mandatory = $false)]
@@ -46,6 +51,75 @@ param (
     [hashtable]
     $ExtendedCAttributes
 )
+function Get-CSRAttributes 
+{
+    $pp_env = Read-Host "pp_environment"
+    $pp_service = Read-Host "pp_service"
+    $pp_role = Read-Host "pp_role"
+
+    $hash = @{
+        pp_service     = $pp_service
+        pp_role        = $pp_role
+        pp_environment = $pp_env
+    }
+    while (!$correct) 
+    {
+        Write-Host "pp_environment: $pp_env`npp_service: $pp_service`npp_role: $pp_role"
+        $correct = Read-Host "Is this correct? [y/n]"
+        switch ($correct)
+        {
+            'y' { Return $hash }
+            'n' 
+            { 
+                $pp_env = Read-Host "pp_environment"
+                $pp_service = Read-Host "pp_service"
+                $pp_role = Read-Host "pp_role"
+                $hash = @{
+                    pp_service     = $pp_service
+                    pp_role        = $pp_role
+                    pp_environment = $pp_env
+                }
+                Clear-Variable correct
+            }
+            default { Clear-Variable correct }
+        }
+    }
+}
+function Set-CSRAttributes
+{
+    param 
+    (
+        [Parameter(Mandatory = $true)]
+        [hashtable]
+        $CSRAttributes
+    )
+    $csr_yaml_path = "$env:ProgramData\PuppetLabs\puppet\etc\csr_attributes.yaml"
+    $csr_yaml_parent = Split-Path $csr_yaml_path
+    if (!(Test-Path $csr_yaml_parent))
+    {
+        try
+        {
+            New-Item $csr_yaml_parent-ItemType Directory -ErrorAction Stop
+        }
+        catch
+        {
+            throw "Failed to create Puppet data directory."
+        }
+    }
+    $csr_yaml = @('extension_requests:')
+    foreach ($attribute in $CSRAttributes.GetEnumerator())
+    {
+        $csr_yaml += "  $($_.Name): $($_.Value)"
+    }
+    try
+    {
+        Set-Content $csr_yaml_path -Value $csr_yaml -ErrorAction Stop
+    }
+    catch
+    {
+        throw "failed to set yaml."
+    }
+}
 if (Get-Command puppet)
 {
     throw "Puppet is already installed"
@@ -54,33 +128,53 @@ if ($PuppetVersion -match "^[1-9]$")
 {
     $PuppetVersion = "puppet$($PuppetVersion)"
 }
-
+# For now we're getting the Puppet agent manually but ultimately I'd like to test getting it via chocolatey - that way we can keep the package up to date.
+$arch = "x86"
 if ( [Environment]::Is64BitOperatingSystem )
 {
-    $DownloadURL = "https://downloads.puppetlabs.com/windows/$($PUPPETVERSION.ToLower())/puppet-agent-x64-latest.msi"
+    $arch = "x64"
 }
-else
+$DownloadURL = "https://downloads.puppetlabs.com/windows/$($PUPPETVERSION.ToLower())/puppet-agent-$arch-latest.msi"
+if ($PuppetAgentVersion -ne 'latest')
 {
-    $DownloadURL = "https://downloads.puppetlabs.com/windows/$($PUPPETVERSION.ToLower())/puppet-agent-x86-latest.msi"
+    $DownloadURL = "https://downloads.puppetlabs.com/windows/$($PUPPETVERSION.ToLower())/puppet-agent-$PuppetAgentVersion-$arch.msi"
 }
 
 $tempname = ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 8 | ForEach-Object { [char]$_ }))
 $tempdir = "$env:TEMP\$tempname"
-try {
+try
+{
     New-Item $tempdir -ItemType Directory -ErrorAction Stop
 }
-catch {
+catch
+{
     
 }
 $installer = "$tempdir\puppet.msi"
-try {
+try
+{
     Invoke-WebRequest -Uri $DownloadURL -OutFile $installer -ErrorAction Stop
 }
-catch {
+catch
+{
     
 }
 if (!$ExtendedCAttributes)
 {
-    $answer = Read-Host "Do you want to set extended CSR attributes? [y/n]"
-
+    while (!$answer)
+    {
+        $answer = Read-Host "Do you want to set extended CSR attributes? [y/n]"
+        switch ($answer.ToLower()) 
+        {
+            'y' 
+            {
+                $ExtendedCAttributes = Get-CSRAttributes
+            }
+            'n' 
+            {   
+                # Do nothing
+            }
+            default { Clear-Variable answer }
+        }
+    }
 }
